3a1d6e5a262376b783d69b42d71ad64ecbb03062
AngelTravieso <angeltraviesoc@gmail.com>
2023-08-14 09:39:29 -0400

    Añadiendo la clase de servicio

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MyService.java
@@ -0,0 +1,9 @@
+package com.bolsadeideas.springboot.di.app.models.service;
+
+public class MyService {
+
+    public String operation() {
+        return "executing something important...";
+    }
+
+}
