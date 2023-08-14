a40dc1fd443c7423869644954f14e75e4d636c29
AngelTravieso <angeltraviesoc@gmail.com>
2023-08-14 09:46:46 -0400

    Implementando inyeccion de dependencias con la notacion Autowired

--- src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.di.app.controllers;
 
 import com.bolsadeideas.springboot.di.app.models.service.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    private MyService service = new MyService();
+    // private MyService service = new MyService();
+
+    // Se inyecta el servicio con la notacion Autowired
+    @Autowired // Esto inyecta un objeto que está registrado en el contenedor de spring (debe estar marcado como @Component o @Service)
+    private MyService service;
 
     @GetMapping({"/","","/index"})
     public String index(Model model) {
--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MyService.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MyService.java
@@ -1,5 +1,11 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+// Entre @Component y @Service la diferencia es semantica, actuan de igual manera
+@Component // -> indicar que mi servicio es un bean de spring, es decir, un componente, esto es un singleton que se puede inyectar en otros componentes
+// @Service // -> Aporta una semantica de que esta clase representa un servicio en spring, una clase de lógica de negocios
 public class MyService {
 
     public String operation() {
