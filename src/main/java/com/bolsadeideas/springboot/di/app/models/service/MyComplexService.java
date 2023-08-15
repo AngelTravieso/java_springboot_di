e16af35475c0869d8fcbd92b27c36637d4772750
AngelTravieso <angeltraviesoc@gmail.com>
2023-08-14 20:32:30 -0400

    Anotacion Primary

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MyComplexService.java
@@ -0,0 +1,16 @@
+package com.bolsadeideas.springboot.di.app.models.service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+// Entre @Component y @Service la diferencia es semántica, actúan de igual manera
+@Component("myComplexService")
+@Primary // Primary permite seleccionar el candidato que se quiere inyectar por defecto en nuestras clases
+public class MyComplexService implements IService {
+
+    @Override // -> este método es una implementación de un padre
+    public String operation() {
+        return "executing complicated process...";
+    }
+
+}
--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MyService.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MyService.java
@@ -3,14 +3,14 @@ package com.bolsadeideas.springboot.di.app.models.service;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-// Entre @Component y @Service la diferencia es semantica, actuan de igual manera
+// Entre @Component y @Service la diferencia es semántica, actúan de igual manera
 @Component("mySimpleService") // -> indicar que mi servicio es un bean de spring, es decir, un componente, esto es un singleton que se puede inyectar en otros componentes
-// @Service // -> Aporta una semantica de que esta clase representa un servicio en spring, una clase de lógica de negocios
+// @Service // -> Aporta una semántica de que esta clase representa un servicio en spring, una clase de lógica de negocios
 public class MyService implements IService {
 
     @Override // -> este método es una implementación de un padre
     public String operation() {
-        return "executing something important";
+        return "executing simple process";
     }
 
 }
